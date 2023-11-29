eb5f180a0e0ef1363db67ceff446749813926c73
Moises Alves <moisesalves1@gmail.com>
2023-11-29 09:54:00 -0300

    Tipando o Retorno da Requisição

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,33 +1,31 @@
 import { Controller, Get, Post, Put, Delete, Param, Body } from "@nestjs/common";
 import { Customer } from "../models/customer.model";
+import { Result } from "../models/result.model";
 
 @Controller('v1/customers')
 export class CustomerController {
     @Get()
     get() {
-        return 'Obter os clientes';
+        return new Result(null, true, [], null);
     }
 
     @Get(':document')
     getById(@Param('document') document: string) {
-        return 'Obter o cliente ' + document;
+        return new Result(null, true, {}, null);
     }
 
     @Post()
     post(@Body() body: Customer) {
-        return body.name;
+        return new Result('Cliente criado com sucesso', true, body, null);
     }
 
     @Put(':document')
     put(@Param('document') document, @Body() body) {
-        return {
-            customer: document,
-            data: body
-        };
+        return new Result('Cliente alterado com sucesso', true, body, null);
     }
 
     @Delete(':document')
     delete(@Param('document') document) {
-        return 'Remover o cliente ' + document;
+        return new Result('Cliente removido com sucesso', true, null, null);
     } 
 }
---
+++ src/backoffice/models/result.model.ts
@@ -0,0 +1,12 @@
+export class Result {
+    
+    constructor(
+        public message: string,
+        public success: boolean,
+        public data: any,
+        public error: any
+    ) {
+    
+        
+    }
+}
