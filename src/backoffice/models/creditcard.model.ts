270f5fb1cacc6c0a8e005e945cf0a0cd259dc6a7
Moises Alves <moisesalves1@gmail.com>
2023-11-29 10:01:30 -0300

    Modelagem

---
+++ src/backoffice/models/creditcard.model.ts
@@ -0,0 +1,12 @@
+export class CreditCard {
+    /**
+     *
+     */
+    constructor(
+        public holder: string,
+        public number: string,
+        public expiration: string,
+    ) {
+        
+    }
+}
