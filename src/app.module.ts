644f52929a5ff5bc2363c6155db9ba4006b1ac07
Moisés Alves <moisesalves1@gmail.com>
2023-12-04 22:45:00 -0300

    TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
 import { StoreModule } from 'src/modules/store/store.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('CONNECTION_STRING', { dbName: 'petshop'}),
+    MongooseModule.forRoot('mongodb+srv://petshop:petshop@petshop.petshop.mongodb.net/?retryWrites=true&w=majority', { dbName: 'petshop'}),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'petshop',
+      password: 'petshop',
+      database: 'petshop',
+      entities: [__dirname + '/**/*.entity{.ts.js}' ],
+      synchronize: true
+    }),
     BackofficeModule,
     StoreModule
   ],
