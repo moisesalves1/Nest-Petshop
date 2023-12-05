40b378524e760be4be0db667af131ca82c7ed932
Moisés Alves <moisesalves1@gmail.com>
2023-12-04 23:52:24 -0300

    Testando o CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
 import { StoreModule } from 'src/modules/store/store.module';
+import { Product } from './modules/store/entities/product.entity';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://petshop:petshop@petshop.petshop.mongodb.net/?retryWrites=true&w=majority', { dbName: 'petshop'}),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
+      host: 'localhost',
       port: 3306,
       username: 'petshop',
-      password: 'petshop',
+      password: 'Petshop@123',
       database: 'petshop',
-      entities: [__dirname + '/**/*.entity{.ts.js}' ],
-      synchronize: true
+      // entities: [__dirname + '/**/*.entity{.ts.js}' ],
+      synchronize: true,
+      entities: [Product]
     }),
     BackofficeModule,
     StoreModule
