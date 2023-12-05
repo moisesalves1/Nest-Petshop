8e6acf9d714ae03843ad0cae3f939ef859c20d3c
Moisés Alves <moisesalves1@gmail.com>
2023-12-04 22:55:01 -0300

    Entidade de Produto

---
+++ src/modules/store/entities/product.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length: 80})
+    title: string
+
+    @Column('text')
+    description: string
+
+}
