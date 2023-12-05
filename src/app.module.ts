b708d441899a06de86d9c7447507f147d084f6d8
Moisés Alves <moisesalves1@gmail.com>
2023-12-04 22:13:27 -0300

    Configurações

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
+import { StoreModule } from 'src/modules/store/store.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('CONNECTION_STRING', { dbName: 'petshop'}),
-    BackofficeModule
+    BackofficeModule,
+    StoreModule
   ],
   controllers: [],
   providers: [],
