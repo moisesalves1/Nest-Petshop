fa60ab8005ea56954496c4083bb2cb33b4603d70
Moises Alves <moisesalves1@gmail.com>
2023-12-05 16:45:19 -0300

    ModulesConfig and TokenTest

--- src/modules/backoffice/backoffice.module.ts
+++ src/modules/backoffice/backoffice.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerController } from 'src/modules/backoffice/controllers/customer.controller';
 import { CustomerSchema } from 'src/modules/backoffice/schemas/customer.schema';
@@ -7,11 +9,21 @@ import { AccountService } from 'src/modules/backoffice/services/account.service'
 import { CustomerService } from 'src/modules/backoffice/services/customer.service';
 import { AddressService } from 'src/modules/backoffice/services/address.service';
 import { PetService } from 'src/modules/backoffice/services/pet.service';
+import { AuthService } from 'src/shared/services/auth.service';
+import { JwtStrategy } from 'src/shared/strategies/jwt.strategy';
 import { AddressController } from 'src/modules/backoffice/controllers/address.controller';
 import { PetController } from 'src/modules/backoffice/controllers/pet.controller';
+import { AccountController } from './controllers/account.controller';
 
 @Module({
     imports: [
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secretOrPrivateKey: '54147f5ce0d2',
+            signOptions: {
+                expiresIn: 3600
+            }
+        }),
         MongooseModule.forFeature([
             {
                 name: 'Customer',
@@ -21,9 +33,9 @@ import { PetController } from 'src/modules/backoffice/controllers/pet.controller
                 name: 'User',
                 schema: UserSchema
             }
-        ])
+        ]),
     ],
-    controllers: [CustomerController, AddressController, PetController],
-    providers: [AccountService, AddressService, CustomerService, PetService],
+    controllers: [AccountController, CustomerController, AddressController, PetController],
+    providers: [AccountService, AddressService, CustomerService, PetService, AuthService, JwtStrategy],
 })
 export class BackofficeModule {}
--- src/modules/backoffice/controllers/account.controller.ts
+++ src/modules/backoffice/controllers/account.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { AuthService } from "src/shared/services/auth.service";
 
 
 @Controller('v1/accounts')
 export class AccountController {
-    constructor() { }
+    constructor(private authService: AuthService) { }
+
+    @Post('')
+    async createToken(): Promise<any> {
+        return await this.authService.createToken();
+    }
 
     @Get('')
     @UseGuards(AuthGuard())
     findAll() {
         return [];
     }
+    
+
 }
