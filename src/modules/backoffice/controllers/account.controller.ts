5e22fe8ee4fbc3309257f5ffa75c12df9e05ddae
Moises Alves <moisesalves1@gmail.com>
2023-12-05 18:06:29 -0300

    AlteraSenha e RefreshToken

--- src/modules/backoffice/controllers/account.controller.ts
+++ src/modules/backoffice/controllers/account.controller.ts
@@ -8,6 +8,7 @@ import { AccountService } from "../services/account.service";
 import { ResultDto } from "../dtos/result.dto";
 import { ResetPasswordDto } from "../dtos/account/reset-password.dto";
 import { Guid } from "guid-typescript";
+import { ChangePasswordDto } from "../dtos/account/change-password.dto";
 
 
 @Controller('v1/accounts')
@@ -43,6 +44,26 @@ export class AccountController {
         } catch (error) {
             throw new HttpException(new ResultDto('Não foi possível restaurar sua senha', false, null, null), HttpStatus.BAD_REQUEST)
         }
+    }
+
+    @Post('change-password')
+    @UseGuards(JwtAuthGuard)
+    async changePassword(@Req() request, @Body() model: ChangePasswordDto): Promise<any> {
+        try {
+            // TODO: Encriptar senha
+            await this.accountService.update(request.user.document, { password: model.password });
+            return new ResultDto('Sua senha foi alterada com sucesso!', true, null, null);
+        } catch (error) {
+            throw new HttpException(new ResultDto('Não foi possível alterar sua senha', false, null, null), HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @Post('refresh')
+    @UseGuards(JwtAuthGuard)
+    async refreshPassword(@Req() request): Promise<any> {
+        // Gera o token
+        const token = await this.authService.createToken(request.user.document, request.user.email, request.user.image, request.user.roles)
+        return new ResultDto(null, true, token, null);
     }
 
 }
