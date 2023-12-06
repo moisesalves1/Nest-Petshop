dab351278c418ee7252e98195681c3ca108c8c5d
Moises Alves <moisesalves1@gmail.com>
2023-12-06 13:30:53 -0300

    Log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
+import { CustomLogger } from './shared/services/custom-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new CustomLogger()
+  });
   await app.listen(3000);
 }
 bootstrap();
