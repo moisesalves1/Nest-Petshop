9458c965d98f788c9ad913a2c4fda9774792f032
Moises Alves <moisesalves1@gmail.com>
2023-12-06 13:38:57 -0300

    Compressão

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import * as compression from "compression";
 import { AppModule } from 'src/app.module';
 import { CustomLogger } from './shared/services/custom-logger.service';
 
@@ -6,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new CustomLogger()
   });
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
