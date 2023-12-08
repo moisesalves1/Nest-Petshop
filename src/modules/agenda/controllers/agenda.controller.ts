2fde72044be67b0e72c55fd18a2f40730e40d9eb
Moises Alves <moisesalves1@gmail.com>
2023-12-08 11:49:05 -0300

    DTOs no CQRS

--- src/modules/agenda/controllers/agenda.controller.ts
+++ src/modules/agenda/controllers/agenda.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { RoomBookService } from "../services/room-book.service";
+import { BookRoomDto } from "../dtos/book-room.dto";
 
 @Controller('v1/rooms')
 export class AgendaController {
     constructor(private readonly service: RoomBookService) { }
 
     @Post()
-    async Book(@Body() body: any) {
+    async Book(@Body() body: BookRoomDto) {
         console.log('AppController:Book - Iniciando a requisição');
         await this.service.Book(body.customer, body.room)
     }
