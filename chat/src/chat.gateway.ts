1421cc0355fc9741869c06e4cb76f7a328e3d658
Moises Alves <moisesalves1@gmail.com>
2023-12-12 11:23:51 -0300

    Novas conexões

--- chat/src/chat.gateway.ts
+++ chat/src/chat.gateway.ts
@@ -1,11 +1,16 @@
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { connected } from "process";
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer()
 
     handleConnection(client: any) {
-        
+        console.log(client.id + ' conectado...');
+        client.broadcast.emit('users', {
+            user: client.id,
+            action: connected
+        })
     }
 
     handleDisconnect(client: any) {
